4d998f79d548d3f8d91d504b5cfac9c7f3ddbc91
ashsidhu <ashmeet.sidhu7@gmail.com>
2014-11-11 12:36:46 -0800

    backbone init

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,44 @@
 // YOUR CODE HERE:
 
+// MODELS
+
+// COLLECTIONS
+var ChatsCollection = Backbone.Collection.extend({
+  url: 'https://api.parse.com/1/classes/chatterbox',
+  initialize: function(){
+    this.fetch();
+    //
+  },
+  parse: function(data) {
+    return data.results;
+  }
+});
+
+// VIEWS
+var ChatsView = Backbone.View.extend({
+  el: '#messages-container',
+  initialize: function(){
+    console.log("chats ")
+
+  }
+});
+
+var InputView = Backbone.View.extend({
+  el: '#message-form',
+  initialize: function(){
+    console.log("input ")
+
+  }
+});
+
+var MainView = Backbone.View.extend({
+  el: '#main',
+  initialize: function(){
+    this.inputView = new InputView();
+    this.chatsView = new ChatsView();
+    this.chatsCollection = new ChatsCollection();
+    console.log("main ")
+  },
+});
+
+var mainView = new MainView();
