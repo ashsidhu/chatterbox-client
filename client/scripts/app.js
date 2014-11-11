348c1df5d41407e5f24319bedd2d87041280f99d
ashsidhu <ashmeet.sidhu7@gmail.com>
2014-11-10 19:47:45 -0800

    add rooms

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,12 +2,19 @@ $(document).on('ready', function() {
 
   // YOUR CODE HERE:
   window.app = {
-    server:'https://api.parse.com/1/classes/chatterbox'
+    server:'https://api.parse.com/1/classes/chatterbox',
+    fetchOptions: {
+      limit: 10,
+      order: '-createdAt'
+    },
+    rooms: [],
+    chatData: []
   };
 
   app.templates = {
     message: _.template("<div><%- username %>: <%- text %></div>"),
-    room: _.template("<div><%- roomname %></div>")
+    room: _.template("<div><%- roomname %></div>"),
+    roomOption: _.template("<option value=<%- index%> ><%- roomname %></option>")
   };
 
   app.init = function() {
@@ -17,9 +24,44 @@ $(document).on('ready', function() {
     $('#reload-chat').on('click', this.reloadChat);
 
     // should fetch existing messages from the server
-    this.fetch(app.renderMessages);
+    this.fetch(app.fetchHandler);
 
     console.log("I'm initiated!");
+  };
+
+// AJAX REQUESTS
+  app.send = function(message) {
+    $.ajax({
+      type: 'POST',
+      url: this.server,
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function(data) {
+        console.log("send success!", data);
+        $('.message-input').val("");
+        app.reloadChat();
+      }
+    });
+  };
+
+  app.fetch = function(successCallback) {
+    // default callback
+    var successCallback = successCallback || function() {};
+    // initiate a new request
+    $.ajax({
+      type: 'GET',
+      url: this.server,
+      data: this.fetchOptions,
+    // pass the results to a callback
+      success: successCallback
+    });
+  };
+
+// DATA HANDLERS
+  app.fetchHandler = function(data) {
+    app.chatData = data.results;
+    app.renderMessages(data.results);
+    app.addRooms().renderRoomSelector()
   };
 
   app.submitForm = function(event){
@@ -33,48 +75,23 @@ $(document).on('ready', function() {
     };
 
     app.send(newMessage);
-    $('.message-input').val("");
-
     // TODO: read later
     // return false;
   };
 
   app.reloadChat = function() {
     app.clearMessages();
-    app.fetch(app.renderMessages);
+    app.fetch(app.fetchHandler);
   };
 
-  app.send = function(message) {
-    $.ajax({
-      type: 'POST',
-      url: this.server,
-      data: JSON.stringify(message),
-      contentType: 'application/json',
-      success: function(data) {
-        console.log("send success!", data);
-      }
-    });
-  };
-
-  app.fetch = function(successCallback) {
-    // default callback
-    successCallback = successCallback || function() {};
-    // initiate a new request
-    $.ajax({
-      type: 'GET',
-      url: this.server,
-    // pass the results to a callback
-      success: successCallback
-    });
-  };
-
-
-  app.renderMessages = function(data) {
+// MESSAGE RENDERING FUNCTIONS
+  app.renderMessages = function(chats) {
     // this bound to request object during callback
     var message;
+    console.log(chats);
 
-    for (var i = 0; i < data.results.length; i++) {
-      message = data.results[i];
+    for (var i = 0; i < chats.length; i++) {
+      message = chats[i];
       app.addMessage(message);
     }
   };
@@ -89,12 +106,35 @@ $(document).on('ready', function() {
 
   app.clearMessages = function() {
     $('#chats').html('');
-  }
-
-  app.addRoom = function(roomname) {
-    var node = $(app.templates.room({roomname: roomname}));
-    $('#roomSelect').append(node);
   };
 
+
+// ROOM FUNCTIONS
+  app.addRoom = function(roomname) {
+    if (!_.contains(app.room, roomname)){
+      app.room.push(roomname);
+      app.renderRoomSelector();
+    }
+  };
+
+  app.addRooms = function(){
+    var chats = app.chatData;
+    app.rooms = [];
+    _.each(chats, function(chat){
+      if (!_.contains(app.rooms, chat.roomname)) {
+        app.rooms.push(chat.roomname);
+      }
+    });
+    return this;
+  };
+
+  app.renderRoomSelector = function() {
+    $('#roomSelect').html('');
+    _.each(app.rooms, function(roomname, index) {
+      var option = $(app.templates.roomOption({roomname: roomname, index: index}));
+      $('#roomSelect').append(option);
+    })
+  }
+
   app.init();
-})
+});
